e8b561342058c893845cc0c54b4dd5c660790770
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2026-07-01 10:22:03 +0300

    First Changes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 // The entry point for your Express application that initializes the server.
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json({ status: 'OK' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
